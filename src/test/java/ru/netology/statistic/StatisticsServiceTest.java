0d6be375731f45a09436853a0641313bd1e808d0
Tatr1 <ttttt_68@mail.ru>
2021-09-06 23:27:10 +0300

    modified test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,15 +7,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
-  @Test
-  void findMax() {
-    StatisticsService service = new StatisticsService();
-
-    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-    long expected = 12;
-
-    long actual = service.findMax(incomesInBillions);
-
-    assertEquals(expected, actual);
-  }
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 11, 8, 4, 5, 13, 8, 6, 11, 11, 12};
+        assertEquals(13, service.findMax(incomesInBillions));
+    }
 }
